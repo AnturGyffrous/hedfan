6fee6c2c6b8063598a512512f52154923d4dd8bf
Owain Williams <owain@hedfan.uk>
2020-04-14 18:30:26 +0100

    Add an AirportTests unit tests class.

---
+++ test/unit/Hedfan.Tests.Unit/Schedules/Airports/AirportTests.cs
@@ -0,0 +1,37 @@
+﻿using AutoFixture;
+
+using FluentAssertions;
+
+using Hedfan.Schedules.Airports;
+
+using Xunit;
+
+namespace Hedfan.Tests.Unit.Schedules.Airports
+{
+    public class AirportTests
+    {
+        public AirportTests()
+        {
+            _fixture = new Fixture();
+
+            _fixture.Customize<Airport>(c => c
+                .With(x => x.Iata, "LTN")
+                .With(x => x.Icao, "EGGW"));
+        }
+
+        private readonly IFixture _fixture;
+
+        [Fact]
+        public void ToStringShouldReturnUserFriendlyAirportDescription()
+        {
+            // Arrange
+            var airport = _fixture.Create<Airport>();
+
+            // Act
+            var result = airport.ToString();
+
+            // Assert
+            result.Should().Be("EGGW London Luton Airport (LTN)");
+        }
+    }
+}
