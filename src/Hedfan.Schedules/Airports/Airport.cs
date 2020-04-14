def27b406ccff6e703c916b7b30a24603ad0bcf4
Owain Williams <owain@hedfan.uk>
2020-04-14 19:21:07 +0100

    Add properties for the other pieces of Airport information.

--- src/Hedfan.Schedules/Airports/Airport.cs
+++ src/Hedfan.Schedules/Airports/Airport.cs
@@ -4,16 +4,37 @@ namespace Hedfan.Schedules.Airports
 {
     public class Airport
     {
+        public int Altitude { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
         public string Iata { get; set; }
 
         public string Icao { get; set; }
 
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
         public string Name { get; set; }
 
+        public string Source { get; set; }
+
+        public string Timezone { get; set; }
+
         protected bool Equals(Airport other) =>
+            string.Equals(Name, other.Name, StringComparison.InvariantCulture) &&
+            string.Equals(City, other.City, StringComparison.InvariantCulture) &&
+            string.Equals(Country, other.Country, StringComparison.InvariantCulture) &&
             string.Equals(Iata, other.Iata, StringComparison.InvariantCulture) &&
             string.Equals(Icao, other.Icao, StringComparison.InvariantCulture) &&
-            string.Equals(Name, other.Name, StringComparison.InvariantCulture);
+            Latitude.Equals(other.Latitude) &&
+            Longitude.Equals(other.Longitude) &&
+            Altitude == other.Altitude &&
+            string.Equals(Timezone, other.Timezone, StringComparison.InvariantCulture) &&
+            string.Equals(Source, other.Source, StringComparison.InvariantCulture);
 
         public override bool Equals(object obj) => ReferenceEquals(this, obj) || obj is Airport other && Equals(other);
 
@@ -21,9 +42,16 @@ namespace Hedfan.Schedules.Airports
         {
             unchecked
             {
-                var hashCode = StringComparer.InvariantCulture.GetHashCode(Iata);
+                var hashCode = StringComparer.InvariantCulture.GetHashCode(Name);
+                hashCode = (hashCode * 397) ^ StringComparer.InvariantCulture.GetHashCode(City);
+                hashCode = (hashCode * 397) ^ StringComparer.InvariantCulture.GetHashCode(Country);
+                hashCode = (hashCode * 397) ^ StringComparer.InvariantCulture.GetHashCode(Iata);
                 hashCode = (hashCode * 397) ^ StringComparer.InvariantCulture.GetHashCode(Icao);
-                hashCode = (hashCode * 397) ^ StringComparer.InvariantCulture.GetHashCode(Name);
+                hashCode = (hashCode * 397) ^ Latitude.GetHashCode();
+                hashCode = (hashCode * 397) ^ Longitude.GetHashCode();
+                hashCode = (hashCode * 397) ^ Altitude.GetHashCode();
+                hashCode = (hashCode * 397) ^ StringComparer.InvariantCulture.GetHashCode(Timezone);
+                hashCode = (hashCode * 397) ^ StringComparer.InvariantCulture.GetHashCode(Source);
                 return hashCode;
             }
         }
--- test/unit/Hedfan.Tests.Unit/Schedules/Airports/AirportTests.cs
+++ test/unit/Hedfan.Tests.Unit/Schedules/Airports/AirportTests.cs
@@ -15,9 +15,17 @@ namespace Hedfan.Tests.Unit.Schedules.Airports
             _fixture = new Fixture();
 
             _fixture.Customize<Airport>(c => c
+                .With(x => x.Name, "London Luton Airport")
+                .With(x => x.City, "London")
+                .With(x => x.Country, "United Kingdom")
                 .With(x => x.Iata, "LTN")
                 .With(x => x.Icao, "EGGW")
-                .With(x => x.Name, "London Luton Airport"));
+                .With(x => x.Latitude, 51.874698638916016)
+                .With(x => x.Longitude, -0.36833301186561584)
+                .With(x => x.Altitude, 526)
+                .With(x => x.Timezone, "Europe/London")
+                .With(x => x.Source, "OurAirports")
+            );
         }
 
         private readonly IFixture _fixture;
@@ -31,7 +39,19 @@ namespace Hedfan.Tests.Unit.Schedules.Airports
         {
             // Arrange
             var airport1 = _fixture.Create<Airport>();
-            var airport2 = new AirportSubClass { Iata = airport1.Iata, Icao = airport1.Icao, Name = airport1.Name };
+            var airport2 = new AirportSubClass
+            {
+                Name = airport1.Name,
+                City = airport1.City,
+                Country = airport1.Country,
+                Iata = airport1.Iata,
+                Icao = airport1.Icao,
+                Longitude = airport1.Longitude,
+                Latitude = airport1.Latitude,
+                Altitude = airport1.Altitude,
+                Timezone = airport1.Timezone,
+                Source = airport1.Source
+            };
 
             // Act
             var result = airport1.Equals(airport2);
