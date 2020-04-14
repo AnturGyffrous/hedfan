106edeff9d417684701cacc368aa3ed99683b998
Owain Williams <owain@hedfan.uk>
2020-04-14 15:12:12 +0100

    Add a FindByIataAsync method to implement the interface.

--- src/Hedfan.Schedules.OpenFlights/Airports/OpenFlightsAirportStore.cs
+++ src/Hedfan.Schedules.OpenFlights/Airports/OpenFlightsAirportStore.cs
@@ -1,6 +1,9 @@
+using System.Threading.Tasks;
+
 namespace Hedfan.Schedules.Airports
 {
     public class OpenFlightsAirportStore : IAirportStore
     {
+        public Task<Airport> FindByIataAsync(string iata) => throw new System.NotImplementedException();
     }
 }
