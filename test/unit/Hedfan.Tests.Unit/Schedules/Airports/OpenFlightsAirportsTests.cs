69c034162e21cbcb4d23c5d383103ba5652aafa0
Owain Williams <owain@hedfan.uk>
2020-04-14 13:29:06 +0100

    Add an OpenFlightsAirportsTests unit test class.

---
+++ test/unit/Hedfan.Tests.Unit/Schedules/Airports/OpenFlightsAirportsTests.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+
+namespace Hedfan.Tests.Unit.Schedules.Airports
+{
+    public class OpenFlightsAirportsTests
+    {
+        public OpenFlightsAirportsTests()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            _fixture.Register<IAirportStore>();
+        }
+
+        private readonly IFixture _fixture;
+    }
+}
