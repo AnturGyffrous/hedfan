7c3f536e9d3b62ada6cee477c4e2a22473c91048
Owain Williams <owain@hedfan.uk>
2020-04-14 23:05:56 +0100

    Make OpenFlightsAirportData implement IEnumerable and read data.

--- src/Hedfan.Schedules.OpenFlights.Data/Airports/OpenFlightsAirportData.cs
+++ src/Hedfan.Schedules.OpenFlights.Data/Airports/OpenFlightsAirportData.cs
@@ -1,6 +1,17 @@
-﻿namespace Hedfan.Schedules.Airports
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using Hedfan.Schedules.Properties;
+
+namespace Hedfan.Schedules.Airports
 {
-    public class OpenFlightsAirportData
+    public class OpenFlightsAirportData : IEnumerable<Airport>
     {
+        private readonly OpenFlightsAirportReader _reader = new OpenFlightsAirportReader(new MemoryStream(Resources.airports));
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IEnumerator<Airport> GetEnumerator() => _reader.GetAirports().GetEnumerator();
     }
 }
---
+++ test/unit/Hedfan.Tests.Unit/Schedules/Airports/OpenFlightsAirportDataTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using AutoFixture;
+
+using FluentAssertions;
+
+using Hedfan.Schedules.Airports;
+
+using Xunit;
+
+namespace Hedfan.Tests.Unit.Schedules.Airports
+{
+    public class OpenFlightsAirportDataTests
+    {
+        public OpenFlightsAirportDataTests()
+        {
+            _fixture = new Fixture();
+
+            _fixture.Inject<IEnumerable<Airport>>(_fixture.Create<OpenFlightsAirportData>());
+
+            _fixture.Register<IAirportStore>(() => _fixture.Create<OpenFlightsAirportStore>());
+        }
+
+        private readonly IFixture _fixture;
+
+        [Fact]
+        public async Task OpenFlightsAirportDataShouldLoadAllAirports()
+        {
+            // Arrange
+            var airportStore = _fixture.Create<IAirportStore>();
+
+            // Act
+            var result = await airportStore.FindByIcaoAsync("YNGU");
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Name.Should().Be("Ngukurr Airport");
+            result.Country.Should().Be("Australia");
+            result.Iata.Should().Be("RPM");
+            result.Latitude.Should().Be(-14.722800254821777);
+            result.Longitude.Should().Be(134.7469940185547);
+            result.Altitude.Should().Be(45);
+        }
+    }
+}
