592009aaff97e07f1cf7c115ba7e037a7097d51b
Owain Williams <owain@hedfan.uk>
2020-04-16 18:26:48 +0100

    Add an OpenFlightsAirportDownloader class and unit test.

---
+++ src/Hedfan.Schedules.OpenFlights/Airports/OpenFlightsAirportDownloader.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Hedfan.Schedules.Airports
+{
+    public static class OpenFlightsAirportDownloader
+    {
+        public static async Task<Stream> Download(HttpClient httpClient)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat");
+
+            var response = await httpClient.SendAsync(request);
+
+            return await response.Content.ReadAsStreamAsync();
+        }
+    }
+}
---
+++ test/unit/Hedfan.Tests.Unit/Schedules/Airports/OpenFlightsAirportDownloaderTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AutoFixture;
+using AutoFixture.AutoMoq;
+
+using FluentAssertions;
+
+using Hedfan.Schedules.Airports;
+using Hedfan.Tests.Unit.Properties;
+
+using Moq;
+using Moq.Protected;
+
+using Xunit;
+
+namespace Hedfan.Tests.Unit.Schedules.Airports
+{
+    public class OpenFlightsAirportDownloaderTests
+    {
+        public OpenFlightsAirportDownloaderTests()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            _fixture.Freeze<Mock<HttpMessageHandler>>()
+                .Protected()
+                .As<ISendAsyncProtectedMembers>()
+                .Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(Encoding.UTF8.GetString(Resources.OpenFlightsAirportDataSample), Encoding.UTF8, "text/plain")
+                });
+
+            _fixture.Register(() => new HttpClient(_fixture.Create<HttpMessageHandler>()));
+        }
+
+        private readonly IFixture _fixture;
+
+        private interface ISendAsyncProtectedMembers
+        {
+            Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken);
+        }
+
+        [Fact]
+        public async Task DownloadShouldUseHardcodedUrlToDownloadLatestData()
+        {
+            // Arrange
+            var stream = await OpenFlightsAirportDownloader.Download(_fixture.Create<HttpClient>());
+            var reader = new OpenFlightsAirportReader(stream);
+            var airportStore = new OpenFlightsAirportStore(reader.GetAirports());
+
+            // Act
+            var result = await airportStore.FindByIcaoAsync("YNGU");
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Name.Should().Be("Ngukurr Airport");
+            result.Country.Should().Be("Australia");
+            result.Iata.Should().Be("RPM");
+            result.Latitude.Should().Be(-14.722800254821777);
+            result.Longitude.Should().Be(134.7469940185547);
+            result.Altitude.Should().Be(45);
+
+            _fixture.Create<Mock<HttpMessageHandler>>()
+                .Protected()
+                .As<ISendAsyncProtectedMembers>()
+                .Verify(x => x.SendAsync(
+                    It.Is<HttpRequestMessage>(m => m.RequestUri == new Uri("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat")),
+                    It.IsAny<CancellationToken>()));
+        }
+    }
+}
