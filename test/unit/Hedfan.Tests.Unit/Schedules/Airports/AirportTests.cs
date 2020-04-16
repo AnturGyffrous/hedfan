b8a713532fe868a06605913589d2179f8c5debae
Owain Williams <owain@hedfan.uk>
2020-04-16 17:13:18 +0100

    Simplify AirportTests by using ExampleAirports.

--- test/unit/Hedfan.Tests.Unit/Schedules/Airports/AirportTests.cs
+++ test/unit/Hedfan.Tests.Unit/Schedules/Airports/AirportTests.cs
@@ -1,8 +1,4 @@
-﻿using AutoFixture;
-
-using FluentAssertions;
-
-using Hedfan.Schedules.Airports;
+﻿using FluentAssertions;
 
 using Xunit;
 
@@ -10,31 +6,11 @@ namespace Hedfan.Tests.Unit.Schedules.Airports
 {
     public class AirportTests
     {
-        public AirportTests()
-        {
-            _fixture = new Fixture();
-
-            _fixture.Customize<Airport>(c => c
-                .With(x => x.Name, "London Luton Airport")
-                .With(x => x.City, "London")
-                .With(x => x.Country, "United Kingdom")
-                .With(x => x.Iata, "LTN")
-                .With(x => x.Icao, "EGGW")
-                .With(x => x.Latitude, 51.874698638916016)
-                .With(x => x.Longitude, -0.36833301186561584)
-                .With(x => x.Altitude, 526)
-                .With(x => x.Timezone, "Europe/London")
-                .With(x => x.Source, "OurAirports")
-            );
-        }
-
-        private readonly IFixture _fixture;
-
         [Fact]
         public void ToStringShouldReturnUserFriendlyAirportDescription()
         {
             // Arrange
-            var airport = _fixture.Create<Airport>();
+            var airport = ExampleAirports.LondonLuton;
 
             // Act
             var result = airport.ToString();
