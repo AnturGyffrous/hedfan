becaaf6aa358e190641c77ecfed3a97b27688f9f
Owain Williams <owain@hedfan.uk>
2020-04-17 14:55:37 +0100

    Add a test for finding the London Luton to Glasgow route

--- test/unit/Hedfan.Tests.Unit/Schedules/Routes/EasyJetRoutesTests.cs
+++ test/unit/Hedfan.Tests.Unit/Schedules/Routes/EasyJetRoutesTests.cs
@@ -1,7 +1,12 @@
-﻿using AutoFixture;
+﻿using System.Linq;
+
+using AutoFixture;
 using AutoFixture.AutoMoq;
 
 using Hedfan.Schedules.Routes;
+using Hedfan.Tests.Unit.Schedules.Airports;
+
+using Xunit;
 
 namespace Hedfan.Tests.Unit.Schedules.Routes
 {
@@ -15,5 +20,17 @@ namespace Hedfan.Tests.Unit.Schedules.Routes
         }
 
         private readonly IFixture _fixture;
+
+        [Fact]
+        public void GetEnumeratorShouldReturnLondonLutonToGlasgowRoute()
+        {
+            // Arrange
+            var routes = _fixture.Create<IRoutes>();
+
+            // Act
+            var route = routes.First(x => x.Origin.Iata == ExampleAirports.LondonLuton.Iata && x.Destination.Iata == ExampleAirports.Glasgow.Iata);
+
+            // Assert
+        }
     }
 }
