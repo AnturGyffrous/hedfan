d8ab0d83e8a33e6064d241ec7013b0ca2a6f9932
Owain Williams <owain@hedfan.uk>
2020-04-17 14:28:26 +0100

    Add an EasyJetRoutesTests class.

---
+++ test/unit/Hedfan.Tests.Unit/Schedules/Routes/EasyJetRoutesTests.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+
+namespace Hedfan.Tests.Unit.Schedules.Routes
+{
+    public class EasyJetRoutesTests
+    {
+        public EasyJetRoutesTests()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            _fixture.Register<IRoutes>(() => _fixture.Create<EasyJetRoutes>());
+        }
+
+        private readonly IFixture _fixture;
+    }
+}
