20404360d859b4a19e4aba2d442258d25b129327
Owain Williams <owain@hedfan.uk>
2020-04-17 17:59:41 +0100

    Add an EasyJetDataProviderTests class.

---
+++ test/unit/Hedfan.Tests.Unit/Schedules/Routes/EasyJetDataProviderTests.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+
+namespace Hedfan.Tests.Unit.Schedules.Routes
+{
+    public class EasyJetDataProviderTests
+    {
+        public EasyJetDataProviderTests()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            _fixture.Register<IEasyJetDataProvider>(() => _fixture.Create<EasyJetDataProvider>());
+        }
+
+        private readonly IFixture _fixture;
+    }
+}
