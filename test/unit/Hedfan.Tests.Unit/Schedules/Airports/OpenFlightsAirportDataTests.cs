9c9e655ba17886ade9b551ae27ebe110e5a94fe7
Owain Williams <owain@hedfan.uk>
2020-04-17 17:15:22 +0100

    Add test for non-generic form of GetEnumerator.

--- test/unit/Hedfan.Tests.Unit/Schedules/Airports/OpenFlightsAirportDataTests.cs
+++ test/unit/Hedfan.Tests.Unit/Schedules/Airports/OpenFlightsAirportDataTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using AutoFixture;
@@ -24,6 +25,41 @@ namespace Hedfan.Tests.Unit.Schedules.Airports
 
         private readonly IFixture _fixture;
 
+        private void AssertResultIsNgukurrAirport(Airport result)
+        {
+            result.Should().NotBeNull();
+            result?.Name.Should().Be("Ngukurr Airport");
+            result?.Country.Should().Be("Australia");
+            result?.Iata.Should().Be("RPM");
+            result?.Latitude.Should().Be(-14.722800254821777);
+            result?.Longitude.Should().Be(134.7469940185547);
+            result?.Altitude.Should().Be(45);
+        }
+
+        [Fact]
+        public void NonGenericGetEnumeratorShouldReturnAllAirports()
+        {
+            // Arrange
+            var airports = _fixture.Create<IEnumerable<Airport>>() as IEnumerable;
+            Airport result = null;
+
+            // Act
+            foreach (var o in airports)
+            {
+                if (o is Airport airport)
+                {
+                    if (airport.Icao == "YNGU")
+                    {
+                        result = airport;
+                        break;
+                    }
+                }
+            }
+
+            // Assert
+            AssertResultIsNgukurrAirport(result);
+        }
+
         [Fact]
         public async Task OpenFlightsAirportDataShouldLoadAllAirports()
         {
@@ -34,13 +70,7 @@ namespace Hedfan.Tests.Unit.Schedules.Airports
             var result = await airportStore.FindByIcaoAsync("YNGU");
 
             // Assert
-            result.Should().NotBeNull();
-            result.Name.Should().Be("Ngukurr Airport");
-            result.Country.Should().Be("Australia");
-            result.Iata.Should().Be("RPM");
-            result.Latitude.Should().Be(-14.722800254821777);
-            result.Longitude.Should().Be(134.7469940185547);
-            result.Altitude.Should().Be(45);
+            AssertResultIsNgukurrAirport(result);
         }
     }
 }
