c7a0ddb44a2b1baa2ed2651d8a5d1c843da27248
Owain Williams <owain@hedfan.uk>
2020-04-17 15:02:08 +0100

    Implement IRoutes interface.

--- src/Hedfan.Schedules.EasyJet/Routes/EasyJetRoutes.cs
+++ src/Hedfan.Schedules.EasyJet/Routes/EasyJetRoutes.cs
@@ -1,6 +1,12 @@
-﻿namespace Hedfan.Schedules.Routes
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Hedfan.Schedules.Routes
 {
     public class EasyJetRoutes : IRoutes
     {
+        public IEnumerator<Route> GetEnumerator() => throw new System.NotImplementedException();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
