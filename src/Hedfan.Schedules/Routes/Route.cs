c46a075d51b73653e047ee13f46d92c7fe1a9282
Owain Williams <owain@hedfan.uk>
2020-04-17 14:59:00 +0100

    Add a Route class.

---
+++ src/Hedfan.Schedules/Routes/Route.cs
@@ -0,0 +1,11 @@
+﻿using Hedfan.Schedules.Airports;
+
+namespace Hedfan.Schedules.Routes
+{
+    public class Route
+    {
+        public Airport Destination { get; set; }
+
+        public Airport Origin { get; set; }
+    }
+}
