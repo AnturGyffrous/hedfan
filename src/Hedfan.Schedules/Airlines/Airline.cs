0f555dacb66022972803b58dc84352cd5f7bd127
Owain Williams <owain@hedfan.uk>
2020-04-18 22:24:33 +0100

    Add an Airline class.

---
+++ src/Hedfan.Schedules/Airlines/Airline.cs
@@ -0,0 +1,15 @@
+﻿namespace Hedfan.Schedules.Airlines
+{
+    public class Airline
+    {
+        public string Callsign { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string Iata { get; set; }
+
+        public string Icao { get; set; }
+
+        public string Name { get; set; }
+    }
+}
