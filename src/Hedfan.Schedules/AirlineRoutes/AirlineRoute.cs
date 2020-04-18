19a1817f1c57e89087cefc05afb04f9c84e39b3d
Owain Williams <owain@hedfan.uk>
2020-04-18 22:25:58 +0100

    Add an Airline property to the AirlineRoute class.

--- src/Hedfan.Schedules/AirlineRoutes/AirlineRoute.cs
+++ src/Hedfan.Schedules/AirlineRoutes/AirlineRoute.cs
@@ -1,9 +1,12 @@
-﻿using Hedfan.Schedules.Airports;
+﻿using Hedfan.Schedules.Airlines;
+using Hedfan.Schedules.Airports;
 
 namespace Hedfan.Schedules.AirlineRoutes
 {
     public class AirlineRoute
     {
+        public Airline Airline { get; set; }
+
         public Airport Destination { get; set; }
 
         public Airport Origin { get; set; }
